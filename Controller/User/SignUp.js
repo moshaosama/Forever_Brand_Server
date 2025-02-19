715bd1187dc9643cc631df24b9f7c06e3c5d542e
moshaosama <mohamedOSFekry@gmail.com>
2025-02-19 22:55:44 +0200

    Latest Version

--- Controller/User/SignUp.js
+++ Controller/User/SignUp.js
@@ -2,17 +2,18 @@ import db from "../../ConnectDB/DB.js";
 import bcrypt from "bcryptjs";
 export const SignUp = async (req, res) => {
   try {
-    const { userName, Email, Password } = req.body;
-    if (!userName || !Email || !Password) {
+    const { userName, Email, Password, Status } = req.body;
+    if ((!userName || !Email || !Password, !Status)) {
       return res.status(404).json({
         statusbar: "danger",
         message: "Please Enter Your Account",
       });
     }
 
-    const Query = "INSERT INTO user(userName, Email, Password) VALUES (?,?,?)";
+    const Query =
+      "INSERT INTO user(userName, Email, Password, Status) VALUES (?,?,?, ?)";
     const hashPassword = await bcrypt.hash(Password, 12);
-    const Values = [userName, Email, hashPassword];
+    const Values = [userName, Email, hashPassword, Status];
 
     const [result] = await db.promise().query(Query, Values);
     return res.status(200).json({
