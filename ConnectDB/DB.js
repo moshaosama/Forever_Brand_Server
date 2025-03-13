dc1aed908bacd6652934bc1d5b2d597bfe6a49a6
moshaosama <mohamedOSFekry@gmail.com>
2025-03-13 23:59:00 +0200

    Edit Env

--- ConnectDB/DB.js
+++ ConnectDB/DB.js
@@ -2,9 +2,9 @@ import mysql from "mysql2";
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
-  user: process.env.DB_USER,
+  user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  database: process.env.DATABASE,
   port: process.env.DB_PORT || 3306,
 });
 
