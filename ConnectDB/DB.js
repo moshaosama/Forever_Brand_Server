0d9e00da753bca98bfc8f0abe2e648e11c06d699
moshaosama <mohamedOSFekry@gmail.com>
2025-03-13 23:54:16 +0200

    Edit Env

--- ConnectDB/DB.js
+++ ConnectDB/DB.js
@@ -1,19 +1,19 @@
-import sql from "mysql2";
+import mysql from "mysql2";
 
-const db = sql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "forever_brand",
-  port: process.env.DB_PORT,
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT || 3306,
 });
 
 db.connect((err) => {
   if (err) {
-    console.error("Failed to connect to the database:", err.message);
+    console.error("❌ Failed to connect to the database:", err.message);
     process.exit(1);
   } else {
-    console.log("Connected to the database successfully");
+    console.log("✅ Connected to the database successfully!");
   }
 });
 
