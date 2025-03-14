524609d2630f30baefc20c4ca5cd3527a9987ed1
moshaosama <mohamedOSFekry@gmail.com>
2025-03-14 15:16:45 +0200

    Latest Versio

--- ConnectDB/DB.js
+++ ConnectDB/DB.js
@@ -3,9 +3,11 @@ import dotenv from "dotenv";
 
 dotenv.config(); // 🛠️ تحميل متغيرات البيئة من ملف .env
 
-const urlDB = `mysql://root:JoDeiyXeDjsNRqNvbOIYtHjQrZNYpeOI@mysql.railway.internal:3306/railway`;
-
-const db = mysql.createConnection(urlDB);
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  database: process.env.DATABASE,
+});
 
 db.connect((err) => {
   if (err) {
