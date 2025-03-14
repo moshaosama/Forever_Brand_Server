0431cc4715b47319cc639c87a0d5d44b7a588574
moshaosama <mohamedOSFekry@gmail.com>
2025-03-14 14:01:45 +0200

    Latest Version

--- ConnectDB/DB.js
+++ ConnectDB/DB.js
@@ -2,7 +2,13 @@ import mysql from "mysql2";
 
 const urlDb = `mysql://root:wsyNFcjUlJygDXSHWgawNWNaFYjQzcWd@mysql.railway.internal:3306/forever_brand`;
 
-const db = mysql.createConnection(urlDb);
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT || 3306,
+});
 
 db.connect((err) => {
   if (err) {
